fb1f18b68080c87d6a744e4b19505412645dd244
Mario Rovito <alessandro.r14@gmail.com>
2022-03-07 17:06:30 +0100

    add const close-hamb

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 
 // add const hamburger-menu
 const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+// add const open-hamburger-menu
+const openHamburgerMenu = document.querySelector(".fas");
+
+// add const close-hamburger-menu
+const closeHamburgerMenu = document.querySelector(".close");
+
+
